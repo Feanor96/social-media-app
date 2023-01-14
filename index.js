9d30461091176b5be9a4b2dc41696d5854f46a8b
Salim <78372105+Feanor96@users.noreply.github.com>
2023-01-15 00:35:47 +0530

    error encountered

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const { MONGO_URI } = require('./config');
+const Post = require('./models/Post');
+const User = require('./models/User');
 
 const typeDefs = gql`
-    type Query {
-        
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
+    type Query { 
+        getPosts: [Post]
     }
 `
+
+const resolvers = {
+    Query: {
+        async getPosts() {
+            try {
+                const posts = await Post.find()
+                return posts;
+            } catch (error) {
+                throw new Error(error)
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true }).then(() => {
+    console.log('DATABASE CONNECTED');
+    return server.listen({ port: 5000 });
+}).then((res) => {
+    console.log(`YOUR API IS RUNNING AT ${res.url}`);
+}).catch(err => {
+    console.error(err);
+});
---
+++ models/User.js
@@ -0,0 +1,11 @@
+const { model, Schema } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: String,
+    password: String,
+    email: String,
+    created_at: Date,
+
+});
+
+module.exports = model('User', UserSchema);
