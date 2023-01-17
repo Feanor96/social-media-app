2df8c20ee5c66efa83d2201f9be28520845ef508
Salim <78372105+Feanor96@users.noreply.github.com>
2023-01-17 23:16:21 +0530

    registering user

--- index.js
+++ index.js
@@ -3,33 +3,9 @@ const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
 const { MONGO_URI } = require('./config');
-const Post = require('./models/Post');
-const User = require('./models/User');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
-const typeDefs = gql`
-    type Post {
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-    }
-    type Query { 
-        getPosts: [Post]
-    }
-`
-
-const resolvers = {
-    Query: {
-        async getPosts() {
-            try {
-                const posts = await Post.find()
-                return posts;
-            } catch (error) {
-                throw new Error(error)
-            }
-        }
-    }
-}
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
